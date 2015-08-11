1ea2b72a5a4770c97b6cd86dab44376810fc8449
Randy Kahle <randy@databliss.net>
2015-08-11 15:17:30 -0400

    Explicit path to gcloud utility

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 docker build -t guestbook  .
+
 docker tag guestbook gcr.io/jenkins-cpo200/guestbook
-gcloud docker push gcr.io/jenkins-cpo200/guestbook
+
+/usr/local/bin/gcloud docker push gcr.io/jenkins-cpo200/guestbook
 
 
