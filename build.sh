44549eec8c2ef2bb78dd7d25112a3d06a035e857
Randy Kahle <randy@databliss.net>
2015-08-14 06:40:24 -0600

    Fetch current project identifier

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
+#! /bin/bash
+#
+# File: build.sh
+#
+# Purpose: Drive the building of a Docker container image
+#
+
+# Issue a request for the metadata service to get the current project identifier
+export PROJECT_ID=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
+
 docker build -t guestbook  .
 
-docker tag guestbook gcr.io/jenkins-cpo200/guestbook
+docker tag guestbook "gcr.io/${PROJECT_ID}/guestbook"
 
-/usr/local/bin/gcloud docker push gcr.io/jenkins-cpo200/guestbook
-
+/usr/local/bin/gcloud docker push "gcr.io/${PROJECT_ID}/guestbook"
 
